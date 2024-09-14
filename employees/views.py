9b9ba684b6c6a897cfad1c15b08ee8e8623aa7fd
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-09-14 11:17:43 +0300

    Исправил баг при создании сотрудника: ввел проверку на повторяющихся сотрудников

--- employees/views.py
+++ employees/views.py
@@ -82,27 +82,34 @@ class EmployeeCreateView(generics.CreateAPIView):
         # Получаем данные из запроса
         position_data = request.data.get("position")
         department_data = request.data.get("department")
+        name = request.data.get("name")
+        surname = request.data.get("surname")
 
         # Проверка на существование должности
-        try:
-            position = Position.objects.get(position=position_data)
-        except Position.DoesNotExist:
+        position = Position.objects.filter(position=position_data).first()
+        if not position:
             raise ValidationError({"position": "Указана несуществующая должность."})
 
         # Проверка на существование отдела
-        try:
-            department = Department.objects.get(department=department_data)
-        except Department.DoesNotExist:
+        department = Department.objects.filter(department=department_data).first()
+        if not department:
             raise ValidationError({"department": "Указан несуществующий отдел."})
+
+        # Проверка на существование сотрудника с той же комбинацией имени и фамилии
+        if Employee.objects.filter(name=name, surname=surname).exists():
+            existing_employee = Employee.objects.get(name=name, surname=surname)
+            raise ValidationError({"detail": f"Сотрудник с именем {name} и фамилией {surname} уже существует. ID: {existing_employee.employee_id}"})
 
         # Сохраняем сотрудника
         employee = employee_serializer.save()
 
-        # Связываем должность с employee_id в таблице должностей
-        Position.objects.filter(position=position_data).update(employee_id=employee.employee_id)
+        # Проверка на существование записи в таблице должностей
+        if not Position.objects.filter(position=position_data, employee_id=employee.employee_id).exists():
+            Position.objects.create(position=position_data, employee_id=employee.employee_id)
 
-        # Связываем отдел с employee_id в таблице отделов
-        Department.objects.filter(department=department_data).update(surname=employee.surname)
+        # Проверка на существование записи в таблице отделов
+        if not Department.objects.filter(department=department_data, position=position_data, surname=surname).exists():
+            Department.objects.create(department=department_data, position=position_data, surname=surname)
 
         # Формируем ответ с данными сотрудника
         response_data = {
